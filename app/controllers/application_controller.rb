74f17cd8c28c9bf59c0676b2d0a03b712a362be3
Corey Donohoe <atmos@atmos.org>
2016-07-13 11:04:55 -0700

    use different auth scopes

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -2,12 +2,16 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
 
+  def boomtown
+    raise("Intentional exception from the web app")
+  end
+
   def health
     render json: { name: "speakerboxxx/the-love-below" }, status: :ok
   end
 
-  def boomtown
-    raise("Intentional exception from the web app")
+  def install
+    redirect_to "/auth/slack?scope=identify,commands,bot"
   end
 
   private
--- config/routes.rb
+++ config/routes.rb
@@ -1,20 +1,20 @@
 Rails.application.routes.draw do
-
   mount Peek::Railtie => "/peek"
 
   get  "/auth/failure",         to: "sessions#destroy"
   get  "/auth/github/callback", to: "sessions#create_github"
   get  "/auth/slack/callback",  to: "sessions#create_slack"
+  post "/signout",  to: "sessions#destroy"
 
-  get "/health", to: "application#health"
+  get "/health",   to: "application#health"
+  get "/install",  to: "application#install"
   get "/boomtown", to: "application#boomtown"
 
-  get "/support", to: "pages#support"
+  get "/support",  to: "pages#support"
 
   post "/webhooks/:team_id/github/:org_name", to: "webhooks#create"
 
   post "/commands", to: "commands#create"
-  post "/signout", to: "sessions#destroy"
 
   root to: "pages#index"
 end
